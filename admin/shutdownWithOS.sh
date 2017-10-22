8265a550be432ff11f32c3d82cae88aa90415d63
tokusato <tokusato0630@gmail.com>
2017-10-22 01:47:16 -0400

    minor fix

--- admin/shutdownWithOS.sh
+++ admin/shutdownWithOS.sh
@@ -7,16 +7,25 @@
 LDLIBPATH=/home/oracle/LDLIB
 OPTION=$1
 
+if [ ! `whoami` = "oracle" ];then
+  echo "execute by root oracle"
+  exit 1
+fi
 
 echo "###################"
 echo "NOW SHUTDOWN ORACLE"
 echo "###################"
 
-${LDLIBPATH}/sqlplus -s sys/oracle@orcl12c as sysdba  << EOF
-
-shutdown immediate;
-
+${LDLIBPATH}/sqlplus -s sys/oracle@orcl12c as sysdba  << EOF 
+  shutdown immediate; 
 EOF
+
+echo sleep
+sleep 10
+
+#${LDLIBPATH}/sqlplus -s sys/oracle@orcl12c as sysdba  << EOF
+#shutdown immediate;
+#EOF
 
 echo "###################"
 echo "NOW SHUTDOWN VIRTUALBOX"
