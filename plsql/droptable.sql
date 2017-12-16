5c7a8b80bc4969dfbf6cdb53737ac02ae05092e4
tokusato <tokusato0630@gmail.com>
2017-12-16 17:55:00 +0900

    IMPDP PERF

---
+++ plsql/droptable.sql
@@ -0,0 +1,25 @@
+DECLARE
+BEGIN
+drop table "TRY"."GRADES";
+drop table "TRY"."SESSIONS";
+drop table "TRY"."DEGREE_LEVELS";
+drop table "TRY"."DEGREE_TYPES";
+drop table "TRY"."DEPARTMENTS";
+drop table "TRY"."TERMS";
+drop table "TRY"."COURSES";
+drop table "TRY"."CLASS_STANDINGS";
+drop table "TRY"."PERFORMANCE_CAPTURE_STATS";
+drop table "TRY"."PERF_STATS";
+drop table "TRY"."STUDENTS_SMALL";
+drop table "TRY"."COURSE_ENROLLMENTS_SMALL";
+drop table "TRY"."APPLICANTS_IMPORT";
+drop table "TRY"."APPLICATIONS";
+drop table "TRY"."APPLICATIONS_ONE_INDEX";
+drop table "TRY"."APPLICATIONS_OVER_INDEXED";
+drop table "TRY"."STUDENTS";
+drop table "TRY"."COURSE_ENROLLMENTS";
+drop table "TRY"."COURSE_OFFERINGS";
+drop table "TRY"."DEGREES";
+drop table "TRY"."DEGREE_REQUIREMENTS";
+drop table "TRY"."STUDENT_CLASS_TERMS";
+END;
