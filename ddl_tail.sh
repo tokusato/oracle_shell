1d014bcecd7fe425afaf5170577137e846316566
root <root@localhost.localdomain>
2017-12-19 23:32:47 +0900

    remove unnecessary files

--- ddl_tail.sh
+++
@@ -1,7 +0,0 @@
-##################################
-## アラートログファイルを出力し続ける。
-##################################
-
-tail -F /u01/app/oracle/diag/rdbms/orcl12c/orcl12c/alert/log.xml
-
-#/u01/app/oracle/diag/rdbms/orcl12c/orcl12c/log/ddl
